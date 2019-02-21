de025769cd1b7645a98dc4f3e2c9792b03e73d8d
evixamar <evixamar@gmail.com>
2019-02-21 13:25:50 -0500

    turning in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+// import moment from 'moment'
 
+import './App.css'
+
+import Movie from './Movie'
+
+import data from './data/party1989.json'
+const baseURL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <header>
+          <h1>party-like-its-1989</h1>
+        </header>
+        <div>
+          {data.results.map((movie, index) => {
+            return (
+              <Movie
+                key={index}
+                title={movie.title}
+                src={`${baseURL}${movie.poster_path}`}
+                overview={movie.overview}
+                // release_date={moment(movie.release_date).format('MMM Do YY')}
+              />
+            )
+          })}
+        </div>
+      </main>
+    )
   }
 }
 
